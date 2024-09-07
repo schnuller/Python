0aa22fcdb6752d119f0dde814154736535105a9c
schnuller <kleine.schnullerbacke+github@gmail.com>
2024-09-07 10:14:40 +0200

    n

--- Test/test.py
+++ Test/test.py
@@ -1,5 +1,21 @@
 import pandas as pd
 from datetime import datetime as dt 
 
-now = dt.now()
-print(now)
+country = {"India": ["Delhi", "Maharashtra", "Haryana", "Uttar Pradesh", "Himachal Pradesh"], "Japan": ["Hokkaido", "Chubu", "Tohoku", "Shikoku", "0"]}
+country_df = pd.DataFrame.from_dict(country)
+
+def main():
+    now = dt.now()
+    print(now)
+    print(country_df)
+    # for key, val in country.items():
+    #     for i in val:
+    #         print(i)
+    # for key, val in country.items():
+    #     for i in val:
+    #         print("{} : {}".format(key, i))
+
+
+
+if __name__ == "__main__":
+    main()
